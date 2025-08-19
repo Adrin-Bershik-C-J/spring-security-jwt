b4fa237da48dd58b7e7cf3ba960ec44549a603c8
cjabe <adrinbershik.2201008@srec.ac.in>
2025-08-19 23:17:34 +0530

    feat:going on with JwtService functionality

---
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long expirationTime;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims=extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return buildToken(claims,userDetails,expirationTime);
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    private String buildToken(Map<String, Object> claims, UserDetails userDetails, long expirationTime) {
+
+    }
+
+}
